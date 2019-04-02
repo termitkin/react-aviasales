18a3896c02c706e72f90fda1ff0b9c47901ac69f
termitkin <bigos911@gmail.com>
2019-04-02 15:28:44 +0300

    changed logo link href

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -5,7 +5,7 @@ import './style.css'
 class Logo extends PureComponent {
   render() {
     return(
-      <a className="logo" href="123.html" title="Главная страница">
+      <a className="logo" href={'https://termitkin.github.io/react-aviasales/'} title="Главная страница">
         <img src = { logo } alt="Лого" />
       </a>
     )
