b0a33371fc05478f4ce3224d822c36f21f7b00a8
termitkin <bigos911@gmail.com>
2019-04-03 19:08:57 +0300

    company logo styles fix

--- src/components/Ticket/companyLogo.js
+++ src/components/Ticket/companyLogo.js
@@ -2,16 +2,16 @@ export function companyLogo(carrier) {
   let className = '';
   
   if ( carrier === 'TK' ) {
-    className = 'turkish-logo ticket__turkish-logo';
+    className = 'turkish-logo ticket__logo';
   }
   else if ( carrier === 'S7' ) {
-    className = 's7-logo ticket__s7-logo';
+    className = 's7-logo ticket__logo';
   }
   else if ( carrier === 'SU' ) {
-    className = 'su-logo ticket__su-logo';
+    className = 'su-logo ticket__logo';
   }
   else if ( carrier === 'BA' ) {
-    className = 'ba-logo ticket__ba-logo';
+    className = 'ba-logo ticket__logo';
   }
   
   return className;
