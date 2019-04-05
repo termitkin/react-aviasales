c81cbf4ba6383cce9051bb9d3ccbe66850615ab0
termitkin <bigos911@gmail.com>
2019-04-05 15:52:05 +0300

    fix format date

--- src/components/Ticket/displayDate.js
+++ src/components/Ticket/displayDate.js
@@ -2,7 +2,7 @@ export function displayDate(str) {
   const days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
   const months = ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'];
   // eslint-disable-next-line
-  const i = str.replace( /(\d+)\.(\d+)\.(\d+)/g, "20" + "$3" + '.' + "$2" + '.' + "$1");
+  const i = str.replace( /(\d+)\.(\d+)\.(\d+)/g, "20" + "$3" + '-' + "$2" + '-' + "$1");
   const d = new Date(i);
 
   return d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear() + ', ' + days[d.getDay()];
