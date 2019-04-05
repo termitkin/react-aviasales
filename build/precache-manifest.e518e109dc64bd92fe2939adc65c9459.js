259843bc373e5e6dfab0f78020b448efff4cf16d
termitkin <bigos911@gmail.com>
2019-04-05 11:11:37 +0300

    rebuild

--- build/precache-manifest.e518e109dc64bd92fe2939adc65c9459.js
+++ build/precache-manifest.e518e109dc64bd92fe2939adc65c9459.js
@@ -16,19 +16,19 @@ self.__precacheManifest = [
     "url": "/react-aviasales/static/js/runtime~main.555fad69.js"
   },
   {
-    "revision": "4d195417709a78234ee4",
-    "url": "/react-aviasales/static/js/main.ac0109fc.chunk.js"
+    "revision": "72f949ec1aa0c5fc50da",
+    "url": "/react-aviasales/static/js/main.bb9b1041.chunk.js"
   },
   {
     "revision": "5057a863b8e4cb3a4ff0",
     "url": "/react-aviasales/static/js/2.62c8be70.chunk.js"
   },
   {
-    "revision": "4d195417709a78234ee4",
-    "url": "/react-aviasales/static/css/main.74bb8501.chunk.css"
+    "revision": "72f949ec1aa0c5fc50da",
+    "url": "/react-aviasales/static/css/main.f2196821.chunk.css"
   },
   {
-    "revision": "a900f9720096ad4a16b45162df48e87a",
+    "revision": "5b1fb1f7119bc5a65abf24582718f736",
     "url": "/react-aviasales/index.html"
   }
 ];
