96c3b68917c674371a7efdc6e35ee6407e197e6d
termitkin <bigos911@gmail.com>
2019-04-05 15:52:29 +0300

    added ticket wrap for shadow under ticket

--- src/components/Ticket/index.jsx
+++ src/components/Ticket/index.jsx
@@ -11,51 +11,55 @@ const Ticket = props => {
 
     const departureDate = displayDate(departure_date);
     const arrivalDate = displayDate(arrival_date);
+    
+    // console.log(arrivalDate);
 
     const finalPrice = priceConverter(price, props.currentCurrency);
 
-    return(
-      <div className='ticket tickets-wrap__ticket' role="article" aria-label="Билет">
-        <div className="ticket__logo-and-buy-button">
-           <div className = { companyLogo(carrier) }></div>
-          <button className="buy-button ticket__buy-button" type="button">
-            <span className="buy-button__text">Купить</span>
-            <span className="buy-button__text">за { finalPrice }</span>
-          </button>
-        </div>
-
-        <div className="ticket__times-and-stops">
-          <div className="ticket__time" aria-label="Время вылета">{ departure_time }</div>
-          <div className="stops">
-            <div className="stops__text" aria-label="Количество пересадок">{ stops ? stops : '' } { stopsText(stops) }</div>
-          </div>
-          <div className="ticket__time" aria-label="Время прилета">{ arrival_time }</div>
-        </div>
-
-        <div className="ticket__route">
-          <div className="ticket__origin">
-            <div className="ticket__airport-and-city" aria-label="Вылет из">
-              <span className="ticket__airport-code">{ origin }, </span>
-              <span className="ticket__city">{ origin_name }</span>
-            </div>
-
-            <div className="ticket__date-and-day" aria-label="Дата вылета">
-              <span className="ticket__date">{ departureDate }</span>
-            </div>
+    return (
+      <div className="wrapForShadowUnderTicket tickets-wrap__ticket">
+        <div className="ticket" role="article" aria-label="Билет">
+          <div className="ticket__logo-and-buy-button">
+             <div className = { companyLogo(carrier) }></div>
+            <button className="buy-button ticket__buy-button" type="button">
+              <span className="buy-button__text">Купить</span>
+              <span className="buy-button__text">за { finalPrice }</span>
+            </button>
           </div>
 
-          <div className="ticket__destination">
-            <div className="ticket__airport-and-city" aria-label="Прилет в">
-              <span className="ticket__city">{ destination_name }, </span>
-              <span className="ticket__airport-code">{ destination }</span>
+          <div className="ticket__times-and-stops">
+            <div className="ticket__time" aria-label="Время вылета">{ departure_time }</div>
+            <div className="stops">
+              <div className="stops__text" aria-label="Количество пересадок">{ stops ? stops : '' } { stopsText(stops) }</div>
+            </div>
+            <div className="ticket__time" aria-label="Время прилета">{ arrival_time }</div>
+          </div>
+
+          <div className="ticket__route">
+            <div className="ticket__origin">
+              <div className="ticket__airport-and-city" aria-label="Вылет из">
+                <span className="ticket__airport-code">{ origin }, </span>
+                <span className="ticket__city">{ origin_name }</span>
+              </div>
+
+              <div className="ticket__date-and-day" aria-label="Дата вылета">
+                <span className="ticket__date">{ departureDate }</span>
+              </div>
             </div>
 
-            <div className="ticket__date-and-day" aria-label="Дата прилета">
-              <span className="ticket__date">{ arrivalDate }</span>
+            <div className="ticket__destination">
+              <div className="ticket__airport-and-city" aria-label="Прилет в">
+                <span className="ticket__city">{ destination_name }, </span>
+                <span className="ticket__airport-code">{ destination }</span>
+              </div>
+
+              <div className="ticket__date-and-day" aria-label="Дата прилета">
+                <span className="ticket__date">{ arrivalDate }</span>
+              </div>
+
             </div>
           </div>
         </div>
-
       </div>
     )
 }
