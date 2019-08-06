e806177b0b11e7d9a7702559f9b65a8d0d27d47e
termitkin <bigos911@gmail.com>
2019-08-06 14:20:01 +0300

    rebuild

--- build/precache-manifest.52a2743f4b48686d0b325d28fd5e8872.js
+++ build/precache-manifest.52a2743f4b48686d0b325d28fd5e8872.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = [
   {
-    "revision": "494da4918bfbcae155064ce0a7512d44",
-    "url": "/react-aviasales/static/media/plane.494da491.svg"
+    revision: "494da4918bfbcae155064ce0a7512d44",
+    url: "/react-aviasales/static/media/plane.494da491.svg"
   },
   {
-    "revision": "eb515c4112576007ef53b00094e8921c",
-    "url": "/react-aviasales/static/media/logo.eb515c41.svg"
+    revision: "eb515c4112576007ef53b00094e8921c",
+    url: "/react-aviasales/static/media/logo.eb515c41.svg"
   },
   {
-    "revision": "b8070e66e3dcab6db60bd9d5005de3e2",
-    "url": "/react-aviasales/static/media/check.b8070e66.svg"
+    revision: "b8070e66e3dcab6db60bd9d5005de3e2",
+    url: "/react-aviasales/static/media/check.b8070e66.svg"
   },
   {
-    "revision": "641fa5560571832df0f7",
-    "url": "/react-aviasales/static/js/runtime~main.555fad69.js"
+    revision: "641fa5560571832df0f7",
+    url: "/react-aviasales/static/js/runtime~main.555fad69.js"
   },
   {
-    "revision": "483b392fdae9f0fa14d5",
-    "url": "/react-aviasales/static/js/main.247c49ca.chunk.js"
+    revision: "483b392fdae9f0fa14d5",
+    url: "/react-aviasales/static/js/main.247c49ca.chunk.js"
   },
   {
-    "revision": "c43a914d50fe89f2dfc8",
-    "url": "/react-aviasales/static/js/2.36092fce.chunk.js"
+    revision: "c43a914d50fe89f2dfc8",
+    url: "/react-aviasales/static/js/2.36092fce.chunk.js"
   },
   {
-    "revision": "483b392fdae9f0fa14d5",
-    "url": "/react-aviasales/static/css/main.9a1a571c.chunk.css"
+    revision: "483b392fdae9f0fa14d5",
+    url: "/react-aviasales/static/css/main.9a1a571c.chunk.css"
   },
   {
-    "revision": "70cf460f9156dcdd7596e743a1304997",
-    "url": "/react-aviasales/index.html"
+    revision: "70cf460f9156dcdd7596e743a1304997",
+    url: "/react-aviasales/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,7 +11,9 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
+);
 
 importScripts(
   "/react-aviasales/precache-manifest.52a2743f4b48686d0b325d28fd5e8872.js"
@@ -29,6 +31,5 @@ workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/react-aviasales/index.html", {
-  
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
 });
